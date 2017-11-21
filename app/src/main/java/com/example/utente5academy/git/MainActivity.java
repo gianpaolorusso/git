b03eb7ba2015729c0ae97bac39a3bcd58ca23437
utente5.academy <gian962001@live.it>
2017-11-21 02:34:12 -0800

    layout

--- app/src/main/java/com/example/utente5academy/git/MainActivity.java
+++ app/src/main/java/com/example/utente5academy/git/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.utente5academy.git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView listapiena;
+    private Button button;
+    private ListView listavuota;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView text=findViewById(R.id.testo);
+        listapiena = findViewById(R.id.listapiena);
+        button = findViewById(R.id.button);
+        listavuota = findViewById(R.id.listavuota);
+
 
     }
 }
